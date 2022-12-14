df5c497b695c6f12956c617761d9ac750b8b9f90
sunil9850 <sunilgadade07@gmail.com>
2022-12-14 12:55:07 +0000

    name java

---
+++ Name.java
@@ -0,0 +1,14 @@
+package testhomework;
+
+public class stringname {
+	
+	public static void main(String[] args)
+		{
+			String s="I love candies";
+			String s1=new String("I love candies");
+			
+			 System.out.println(s1+" "+s);
+			
+		}
+}
+
